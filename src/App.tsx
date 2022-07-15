30c753224368bdbffb4659b513158ea1385ef935
Alexey Egorov <krosy1337@gmail.com>
2022-07-15 14:58:01 +0300

    Add routing
    Add placeholders for "Home page" and "Favourites page"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react"
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import FavouritesPage from "./pages/FavouritesPage"
 
 function App() {
     return (
-        <div>
-
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/favourites" element={<FavouritesPage />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
---
+++ src/pages/FavouritesPage.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from "react"
+
+const FavouritesPage: FC = () => {
+    return (
+        <div>
+            Favourites Page
+        </div>
+    )
+}
+
+export default FavouritesPage
