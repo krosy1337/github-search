2da9c1ed685ad7fee2a055063165f2e5156408f0
Alexey Egorov <krosy1337@gmail.com>
2022-07-15 17:17:58 +0300

    Refactor routes
    Add enum RoutesNames in routes.ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import React from "react"
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import FavouritesPage from "./pages/FavouritesPage"
+import Header from "./components/Header"
+import {CssBaseline} from "@mui/material"
+import { RoutesNames } from "./routes"
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/favourites" element={<FavouritesPage />} />
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <CssBaseline />
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path={RoutesNames.ROOT} element={<HomePage />} />
+                    <Route path={RoutesNames.FAVOURITES} element={<FavouritesPage />} />
+                    <Route path="*" element={<Navigate to={RoutesNames.ROOT} />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
---
+++ src/routes.ts
@@ -0,0 +1,4 @@
+export enum RoutesNames {
+    ROOT = "/",
+    FAVOURITES = "FAVOURITES",
+}
