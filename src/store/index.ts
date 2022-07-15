4193418b491780b568e752700923d3cac959ed34
Alexey Egorov <krosy1337@gmail.com>
2022-07-15 17:52:03 +0300

    Update store for new github APi

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,12 @@
-import {configureStore, createStore} from "@reduxjs/toolkit"
+import {configureStore} from "@reduxjs/toolkit"
+import {githubApi} from "./github/github.api"
+import {setupListeners} from "@reduxjs/toolkit/query"
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [githubApi.reducerPath]: githubApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware),
 })
+
+setupListeners(store.dispatch)
