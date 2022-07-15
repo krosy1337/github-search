e80a69d4297721449e30f5103e10bb731d39a49d
Alexey Egorov <krosy1337@gmail.com>
2022-07-15 18:42:28 +0300

    Fix useDebounce

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,14 @@
+import {useEffect, useState} from "react"
+
+export const useDebounce = (value: any, delay: number = 300) => {
+    const [debouncedValue, setDebouncedValue] = useState<any>(null)
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value)
+        }, delay)
+        return () => clearTimeout(handler)
+    }, [value, delay])
+
+    return debouncedValue
+}
