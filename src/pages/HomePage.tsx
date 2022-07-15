2d8ed6b682eb53dff48fa843e1eee02a927fc426
Alexey Egorov <krosy1337@gmail.com>
2022-07-15 22:25:35 +0300

    Add onFocus and onBlur events for input

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,16 +5,22 @@ import CircularProgress from "@mui/material/CircularProgress"
 import {useDebounce} from "../hooks/useDebounce"
 import UserCardItem from "../components/UserCardItem"
 import {IUser} from "../models/github"
+import Typography from "@mui/material/Typography"
 
 const HomePage: FC = () => {
     const [username, setUsername] = useState<string>("")
+    const [isUsersVisible, setUsersVisible] = useState<boolean>(false)
     const debouncedUsername = useDebounce(username)
     const {isLoading, isError, data} = useSearchUsersQuery(debouncedUsername, {
-        skip: !debouncedUsername
+        skip: !debouncedUsername,
+        refetchOnFocus: true,
     })
 
+    useEffect(() => {
+        setUsersVisible(debouncedUsername && data)
+    }, [debouncedUsername, data])
     const onClick = (login: string) => {
-
+        setUsersVisible(false)
     }
 
     return (
@@ -25,21 +31,38 @@ const HomePage: FC = () => {
             <Box sx={{position: "relative"}}>
                 <TextField variant="standard" placeholder="Search users on Github"
                            value={username} onChange={(e) => setUsername(e.currentTarget.value)}
+                           onFocus={() => {
+                               if (username) {
+                                   setUsersVisible(!isError)
+                               }
+                           }}
+                           onBlur={() => {
+                               setUsersVisible(false)
+                           }}
                            sx={{height: 32, width: 300}}/>
-                <Paper component={List} elevation={10}
-                       sx={{
-                           position: "absolute", top: 32,
-                           height: 300, width: "100%",
-                           m: 0, p: 0,
-                           overflowY: "scroll"
-                       }}>
-                    {isLoading && <Box
-                        sx={{display: "flex", justifyContent: "center", marginTop: 2,}}><CircularProgress/></Box>}
-                    {data?.map((user: IUser) =>
-                        <UserCardItem key={user.id} login={user.login} avatar_url={user.avatar_url} onClick={onClick}/>
-                    )}
-                </Paper>
+                {
+                    isUsersVisible &&
+                    <Paper component={List} elevation={10}
+                           sx={{
+                               position: "absolute", top: 32,
+                               height: 300, width: "100%",
+                               m: 0, p: 0,
+                               overflowY: "scroll"
+                           }}>
+                        {isLoading && <Box
+                            sx={{display: "flex", justifyContent: "center", marginTop: 2,}}><CircularProgress/></Box>}
+                        {(data && data.length > 0)
+                            ?
+                            data?.map((user: IUser) =>
+                                <UserCardItem key={user.id} login={user.login} avatar_url={user.avatar_url}
+                                              onClick={onClick}/>)
+                            :
+                            <Typography variant="h5" align="center">No one user :(</Typography>
+                        }
+                    </Paper>
+                }
             </Box>
+            {isError && <Typography variant="h3" align="center">Something were wrong...</Typography>}
         </Container>
     )
 }
