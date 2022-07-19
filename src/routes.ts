cc4419fcc19b9ea545d944199d63b0859f8ae4ff
Alexey Egorov <krosy1337@gmail.com>
2022-07-19 15:08:57 +0300

    Add rootRoutes and guestRoutes in routes.tsx

--- src/routes.ts
+++
@@ -1,6 +0,0 @@
-export enum RoutesNames {
-    ROOT = "/",
-    FAVOURITES = "/FAVOURITES",
-    LOGIN = "/LOGIN",
-    REGISTER = "/REGISTER",
-}
---
+++ src/routes.tsx
@@ -0,0 +1,38 @@
+import {Navigate, RouteObject} from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import FavouritesPage from "./pages/FavouritesPage"
+import RegisterPage from "./pages/RegisterPage"
+import LoginPage from "./pages/LoginPage"
+import React from "react"
+
+export enum RoutesNames {
+    ROOT = "/",
+    FAVOURITES = "/FAVOURITES",
+    LOGIN = "/LOGIN",
+    REGISTER = "/REGISTER",
+}
+
+export const rootRoutes: RouteObject[] = [
+    {
+        path: RoutesNames.ROOT,
+        element: <HomePage/>,
+    },
+    {
+        path: RoutesNames.FAVOURITES,
+        element: <FavouritesPage/>,
+    },
+    {
+        path: "*",
+        element: <Navigate to={RoutesNames.ROOT}/>,
+    }
+]
+export const guestRoutes: RouteObject[] = [
+    {
+        path: RoutesNames.REGISTER,
+        element: <RegisterPage/>,
+    },
+    {
+        path: RoutesNames.LOGIN,
+        element: <LoginPage/>,
+    },
+]
